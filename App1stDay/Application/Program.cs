f00069b1d6da348b2f4ae9d6c5d06b427d331fe0
claudiu-orosanu <orosanu.claudiu@gmail.com>
2016-06-30 21:01:03 +0300

    first app

---
+++ App1stDay/Application/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader file = new StreamReader(@"../../in.txt");
+            string line;
+            string[] words;
+            Dictionary<string, int> table = new Dictionary<string, int>();
+            while ((line = file.ReadLine()) != null)
+            {
+                words = Regex.Split(line, "[ ,.;\n!?]+");
+                foreach (string word in words)
+                {
+                    if (table.ContainsKey(word))
+                        table[word] += 1;
+                    else
+                        table.Add(word, 1);
+                }
+            }
+
+            foreach (var pair in table)
+            {
+                Console.WriteLine("Cuvantul \"{0}\" are {1} aparitii", pair.Key,pair.Value);
+            }
+        }
+    }
+}
