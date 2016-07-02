89db271dae794e60af9fb95f670afc35f8507ef3
claudiu-orosanu <orosanu.claudiu@gmail.com>
2016-07-02 13:23:45 +0300

    MyHomework

---
+++ MyHomework/MyHomework/Leave.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomework
+{
+    class Leave
+    {
+        public string startingDate;
+        public int duration;  //nr de zile
+        public string leaveType;
+        public Employee employee;
+
+        public Leave(string StartingDate, int Duration, string LeaveType)
+        {
+            startingDate = StartingDate;
+            duration = Duration;
+            leaveType = LeaveType;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Starting date: {0}, Duration: {1}, Leave type: {2}", startingDate, duration, leaveType);
+        }
+    }
+}
---
+++ MyHomework/MyHomework/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomework
+{
+    class Person
+    {
+        protected string firstname;
+        protected string lastname;
+        protected string dateOfBirth;
+
+    }
+}
